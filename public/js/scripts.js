a0b09f4e81dc1f9663e05401cda6890d09cc3f54
Sam <mess79@gmail.com>
2019-02-26 21:49:40 +0000

    styling

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,26 +1,88 @@
 $(function() {
 
-  const id = "5c327a5589ecdd4af7821c6a"
-  const deleteId = "5c328284408b6250b1788523"
+  let ids = function() {
+
+    let obj = {
+      id: $(".id").val(),
+      account: $(".account").val(),
+      order: $(".order").val(),
+      delete_id: $(".delete_id").val()
+    }
+    return obj;
+  }
+  
   const obj = {
     "first_name": "test",
     "surname": "case2",
-    "china" : {
-      "previous_nationality" : "FRE"
+    "china": {
+      "previous_nationality": "FRE"
     },
-    "india" : {
-      "previous_nationality" : "USA"
+    "india": {
+      "previous_nationality": "USA"
     }
   }
 
-  $("#get").on('click', function(event) {
+  $("#collection").on("change", function(event) {
+    switch ($(this).val()) {
+      case "order":
+        urls = {
+          get: "/order/" + ids().order,
+          post: "/order",
+          put: "/order",
+          patch: "/order",
+          delete: "/order"
+        }
+        break
+      case "account":
+        urls = {
+          get: "#",
+          post: "#",
+          put: "#",
+          patch: "#",
+          delete: "#"
+        }
+        break
+      case "invoice":
+        urls = {
+          get: "#",
+          post: "#",
+          put: "#",
+          patch: "#",
+          delete: "#"
+        }
+        break
+      case "person_country":
+        urls = {
+          get: "/person_country/options/country/china/id/" + ids().id,
+          post: "/person_country/options/country/india/id/" + ids().id,
+          put: "/person_country",
+          patch: "/person_country/options/country/china/",
+          delete: "/person_country/options/id/" + ids().delete_id
+        }
+        break
+      case "company":
+        urls = {
+          get: "#",
+          post: "#",
+          put: "#",
+          patch: "#",
+          delete: "#"
+        }
+        break
+    }
+  })
+
+  $("#collection").trigger("change");
+
+  $(".get").on('click', function(event) {
     event.preventDefault();
     $.ajax({
-      url: "/api/options/country/china/id/" + id,
+      url: urls.get,
       type: 'GET',
-      //data: JSON.stringify(obj),
       success: function(data) {
         console.log(data);
+        $(".url").text(urls.get);
+        $(".output").text(JSON.stringify(data, null, 4));
       },
       cache: false,
       contentType: "application/json",
@@ -28,14 +90,16 @@ $(function() {
     });
   })
 
-  $("#post").on('click', function(event) {
+  $(".post").on('click', function(event) {
     event.preventDefault();
     $.ajax({
-      url: "/api/options/country/india/id/" + id,
+      url: urls.post,
       type: 'POST',
       data: JSON.stringify(obj),
       success: function(data) {
         console.log(data);
+        $(".url").text(urls.post);
+        $(".output").text(JSON.stringify(data, null, 4));
       },
       cache: false,
       contentType: "application/json",
@@ -43,14 +107,16 @@ $(function() {
     });
   })
 
-  $("#patch").on('click', function(event) {
+  $(".patch").on('click', function(event) {
     event.preventDefault();
     $.ajax({
-      url: "/api",
+      url: urls.patch,
       type: 'PATCH',
       data: JSON.stringify(obj),
       success: function(data) {
         console.log(data);
+        $(".url").text(urls.patch);
+        $(".output").text(JSON.stringify(data, null, 4));
       },
       cache: false,
       contentType: "application/json",
@@ -58,14 +124,16 @@ $(function() {
     });
   })
 
-  $("#put").on('click', function(event) {
+  $(".put").on('click', function(event) {
     event.preventDefault();
     $.ajax({
-      url: "/api/options/country/china/",
+      url: urls.put,
       type: 'PUT',
       data: JSON.stringify(obj),
       success: function(data) {
         console.log(data);
+        $(".url").text(urls.put);
+        $(".output").text(JSON.stringify(data, null, 4));
       },
       cache: false,
       contentType: "application/json",
@@ -73,20 +141,19 @@ $(function() {
     });
   })
 
-  $("#delete").on('click', function(event) {
+  $(".delete").on('click', function(event) {
     event.preventDefault();
     $.ajax({
-      url: "/api/options/id/" + deleteId,
+      url: urls.delete,
       type: 'DELETE',
-      //data: JSON.stringify(obj),
       success: function(data) {
         console.log(data);
+        $(".url").text(urls.delete);
+        $(".output").text(JSON.stringify(data, null, 4));
       },
       cache: false,
       contentType: "application/json",
       processData: false
     });
   })
-
-
 })
