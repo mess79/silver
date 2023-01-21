0f0620d4c90ec888d45d7d9fe52f5ec571e52108
mess79 <mess79@gmail.com>
2023-01-21 18:32:34 +0000

    Restart commit

--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 //const url = process.env.MONGO || "mongodb://root:vegetable@127.0.0.1:27017/autoFiller";
 const url = process.env.MONGO || "mongodb+srv://mess79:vegetable@cluster0-jrnel.mongodb.net/test?retryWrites=true&w=majority";
 const options = { useNewUrlParser: true };
-mongoose.set('useFindAndModify', false);
+//mongoose.set('useFindAndModify', false);
 
 mongoose.connect(url, options).then(
   () => {
